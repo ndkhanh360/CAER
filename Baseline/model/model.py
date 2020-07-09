2242e3ddd3bdca597c9afe351225d67932b347c5
ndkhanh360 <duykhanhnguyen360@gmail.com>
2020-07-09 21:13:30 +0700

    add pretrain resnet

--- Baseline/model/model.py
+++ Baseline/model/model.py
@@ -1,19 +1,36 @@
 import torch.nn as nn
+import torch
 import torch.nn.functional as F
 from base import BaseModel
 from torchvision import models
 
 class ResNet(BaseModel):
-    def __init__(self, drop_out=False, num_classes=7, fine_tune=True):
+    def __init__(self, drop_out=False, num_classes=7, fine_tune=True, path=None):
         super().__init__()
-        self.model = models.resnet152(pretrained=True)
+        if path is not None:
+            print('Loading checkpoints from pretrained...')
+            checkpoint = torch.load(path)
+            self.model = models.resnet18()
 
-        if not fine_tune:
-            for param in self.model.parameters():
-                param.requires_grad = False 
+            pretrained_state_dict = checkpoint['state_dict']
+            model_state_dict = self.model.state_dict()
+            for key in pretrained_state_dict:
+                if  ((key=='module.fc.weight')|(key=='module.fc.bias')):
+                    pass
+                else:    
+                    new_key = key[7:]
+                    if new_key in model_state_dict.keys():
+                        model_state_dict[new_key] = pretrained_state_dict[key]
+            self.model.load_state_dict(model_state_dict)  
+        else:
+            self.model = models.resnet152(pretrained=True)
+          
+
+        for param in self.model.parameters():
+            param.requires_grad = fine_tune ## fix this 
                 
         num_features = self.model.fc.in_features
-        self.fc = nn.Sequential(
+        self.model.fc = nn.Sequential(
             nn.Dropout(0.5),
             nn.Linear(num_features, num_classes)
         ) if drop_out else nn.Linear(num_features, num_classes)
